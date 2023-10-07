97b68a062f6f79114451914db1ed77f1dbc4f225
Fyodor Antonevich <Levity537@gmail.com>
2023-10-07 22:57:18 +0300

    v1

--- vacation_service/users/views.py
+++ vacation_service/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import User
+
+
+def index(request):
+    template = 'users/index.html'
+    post_list = User.objects.select_related(
+    ).all()
+    context = {
+        # 'page_obj': posts_per_page(request, post_list),
+    }
+    return render(request, template, context)
--- vacation_service/vacation_service/urls.py
+++ vacation_service/vacation_service/urls.py
@@ -1,7 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
 ]
